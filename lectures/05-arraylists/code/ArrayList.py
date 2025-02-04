520a6d0586c01438db9cc64280e4872876eb851e
Sebastián <src402@nyu.edu>
2025-02-03 20:27:00 -0500

    first draft of arraylist

--- lectures/05-arraylists/code/ArrayList.py
+++ lectures/05-arraylists/code/ArrayList.py
@@ -3,40 +3,47 @@ from ctypes import py_object
 
 def make_array(size):
     """
-    Returns a C-style low-level array of a specified size.
-
+    Returns a low-level C-style array of the specified size.
+    
     Args:
-        size (int): The desired size of the array
-
+        size (int): Desired size of the array.
     Returns:
-        Array: A low-level array
+        Array: A new low-level array of the specified size.
     """
     return (size * py_object)()
 
 
 class ArrayList:
     def __init__(self):
-        self.data_arr = make_array(1)
+        self.data_arr = make_array(1)  # Start with capacity for 1 element
         self.capacity = 1
-        self.size = 1
+        self.size = 0  # Track the current number of elements
 
     def append(self, value):
-        pass
+        if self.size == self.capacity:  # Need to resize
+            self.resize(self.capacity * 2)
+        self.data_arr[self.size] = value
+        self.size += 1
 
-    def resize(self, new_size):
-        pass
-
-    def extend(self, iter_collection):
-        pass
+    def resize(self, new_capacity):
+        """
+        Resize the array to a new capacity.
+        """
+        new_arr = make_array(new_capacity)
+        for i in range(self.size):
+            new_arr[i] = self.data_arr[i]
+        self.data_arr = new_arr
+        self.capacity = new_capacity
 
     def __len__(self):
-        pass
+        return self.size
 
     def __getitem__(self, idx):
-        pass
+        if not 0 <= idx < self.size:
+            raise IndexError("Index out of range")
+        return self.data_arr[idx]
 
     def __setitem__(self, idx, value):
-        pass
-
-    def __iter__(self):
-        pass
+        if not 0 <= idx < self.size:
+            raise IndexError("Index out of range")
+        self.data_arr[idx] = value
